ff53a95c9a8acc3f88448f46ba558ed62b9ff07e
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-02-07 03:06:19 -0500

    Pimer avance

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function crearTablero()
+{
+var crearTablero=document.getElementById('crearTablero');
+crearTablero.innerHTML='<div class="col-md-8 divCrear"><h4>Crear Tablero</h4>'+
+							'<hr><p><strong>Ttitulo</strong></p>'+
+							'<input type="" name="" placeholder="Como Cosas de casa, por ejemplo ...">'+
+							'<br><br>'+
+							'<button>Crear</button>'+
+						'</div>';
+}
