01d511f7a45ebf909a225ffa1968784723ebf8f1
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-02-15 06:59:41 -0500

    Añadi colores aleatorios

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 var isCreate = false;//
 var display = false;
+
+
+
 function crearTablero()
 {
 
-	var listaTableros= document.getElementById('listaTableros');
+	var crearTab= document.getElementById('crearTab');
 	if(!isCreate){//si aun no se ha creado lo creo
 		
-		listaTableros.setAttribute("style", "block");
+		crearTab.setAttribute("style", "block");
 		
 		var divCrear='<p class="text-center"><strong>Crear Tablero<strong></p><hr>'+
 					'<label>Titulo</label><br>'+
 					'<input type="text" id="inputTablero" name="" placeholder="  <<nombre del tablero>>"><br>'+
 					'<button id="btnCrearTab">Crear Tablero</button>';
-		listaTableros.innerHTML= divCrear;
-		inputTablero.focus();	
+		crearTab.innerHTML= divCrear;	
 		btnCrearTab.addEventListener('click',aniadirTablero);
 		isCreate=true;
 		
@@ -22,18 +24,39 @@ function crearTablero()
 	{
 		if(!display)
 		{
-			listaTableros.style.display= "block";
+			crearTab.style.display= "block";
 			display=true;
 		}
 		else
 		{
-			listaTableros.style.display= "none";
+			crearTab.style.display= "none";
 			display=false;
 		}
 	}
+
 	
 }
 function aniadirTablero()
 {
-
+	//console.log(inputTablero.value);
+	sessionStorage.setItem('tablero',inputTablero.value);
+	crearTab.style.display = 'none';
+	for(var i=0;i<sessionStorage.length;i++){
+        var tablero=sessionStorage.key(i);
+        var nombre=sessionStorage.getItem(tablero);
+        listaTablero.innerHTML += '<li class="list-unstyled elemento" id="elemento"><small class="text-left">'+nombre+'</small></li>';
+    }
+    
+    var color='#'+Math.floor(Math.random()*16777215).toString(16);
+    var elemento=document.getElementById("elemento");
+    elemento.style.backgroundColor= color;
+    inputTablero.value="";
+    inputTablero.focus();
+}
+function colores()
+{
+	var color='#'+Math.floor(Math.random()*16777215).toString(16);
+	console.log(color);
+	var colores=document.getElementById("colores");
+	colores.style.backgroundColor= color;
 }
